4bc14f2eeb7da9da99d9c0cd6555210858dae693
antlord <antlord92@gmail.com>
2016-08-19 02:42:51 +0800

    init commit

---
+++ resonancesml/__main__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+
+def main():
+    print(123)
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='resonances-ml',
+    version='',
+    packages=['resonancesml'],
+    url='',
+    license='',
+    author='ANtlord',
+    author_email='',
+    description=''
+)
