ffee1a8a5480b08ed9adc5ed49b19bf8d4a2df86
antlord <antlord92@gmail.com>
2016-08-29 16:52:45 +0800

    added classifying by allnum.cat

--- resonancesml/commands/learn.py
+++ resonancesml/commands/learn.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 from resonancesml.shortcuts import ProgressBar
 from texttable import Texttable
 from typing import Tuple
@@ -63,20 +64,6 @@ def _classify(clf: ClassifierMixin, kf: cross_validation.KFold, X: np.ndarray, Y
             np.sum(TPs), np.sum(FPs), np.sum(TNs), np.sum(FNs))
 
 
-def _get_headers(from_catalog_path:str, by_indices: List[int]) -> List[str]:
-    headers = []
-    with open(from_catalog_path) as f:
-        for i, line in enumerate(f):
-            if i == 1:
-                headers = [x.strip() for x in line.split('  ') if x]
-            elif i > 1:
-                break
-    res = []
-    for index in by_indices:
-        res.append(headers[index])
-    return res
-
-
 def _perf_measure(y_actual, y_hat):
     TP = 0
     FP = 0
@@ -107,16 +94,18 @@ class Catalog(Enum):
 
 class TesterParameters:
     def __init__(self, indices_cases: List[List[int]], catalog_path: str,
-                 catalog_width: int):
+                 catalog_width: int, delimiter: str, skiprows: int):
         self.indices_cases = indices_cases
         self.catalog_path = catalog_path
         self.catalog_width = catalog_width
+        self.delimiter = delimiter
+        self.skiprows = skiprows
 
 
 def get_tester_parameters(catalog: Catalog) -> TesterParameters:
     return {
-        Catalog.syn: TesterParameters([[2,3,4],[2,3,4,5]], SYN_CATALOG_PATH, 10),
-        Catalog.cat: TesterParameters([[2,3,4],[2,3,4,5]], CAT_CATALOG_PATH, 11),
+        Catalog.syn: TesterParameters([[2,3,4],[2,3,4,5]], SYN_CATALOG_PATH, 10, '  ', 2),
+        Catalog.cat: TesterParameters([[2,3,4,5,6,7]], CAT_CATALOG_PATH, 11, ', ', 6),
     }[catalog]
 
 
@@ -125,40 +114,52 @@ class MethodComparer:
         dtype = {0:str}
         dtype.update({x: float for x in range(1,parameters.catalog_width)})
         self._catalog_feautures = pandas.read_csv(  # type: DataFrame
-            parameters.catalog_path, delim_whitespace=True, skiprows=2, header=None, dtype=dtype)
+            parameters.catalog_path, delim_whitespace=True,
+            skiprows=parameters.skiprows, header=None, dtype=dtype)
         self._librated_asteroids = np.loadtxt(librate_list, dtype=int)
-        self._indices_cases = parameters.indices_cases
-        self._catalog_path = parameters.catalog_path
+        self._parameters = parameters
+
+    def _get_headers(self, by_indices: List[int]) -> List[str]:
+        headers = []
+        header_line_number = self._parameters.skiprows - 1
+        with open(self._parameters.catalog_path) as f:
+            for i, line in enumerate(f):
+                if i == header_line_number:
+                    headers = [x.strip() for x in line.split(self._parameters.delimiter) if x]
+                elif i > header_line_number:
+                    break
+        res = []
+        for index in by_indices:
+            res.append(headers[index])
+        return res
 
     def learn(self):
         slice_len = int(self._librated_asteroids[-1])
-        dtype = {0:str}
-        dtype.update({x: float for x in range(1,10)})
-        syntetic_elems = pandas.read_csv(self._catalog_path, delim_whitespace=True,  # type: DataFrame
-                                         skiprows=2, header=None, dtype=dtype)
 
-        learn_feature_set = syntetic_elems.values[:slice_len]  # type: np.ndarray
+        learn_feature_set = self._catalog_feautures.values[:slice_len]  # type: np.ndarray
         table = Texttable(max_width=120)
         table.header(['Classifier', 'Input data (fields)', 'precision', 'recall',
                       'accuracy', 'TP', 'FP', 'TN', 'FN'])
         table.set_cols_width([30, 30, 15, 15, 15, 5, 5, 5, 5])
         table.set_precision(5)
 
-        bar = ProgressBar(len(self._indices_cases) * 5, 'Learning', 1)
+        bar = ProgressBar(len(self._parameters.indices_cases) * 5, 'Learning', 1)
+        Y = get_target_vector(self._librated_asteroids, learn_feature_set.astype(int))
 
-        for indices in self._indices_cases:
+        for indices in self._parameters.indices_cases:
             classifiers = {
                 'Decision tree': DecisionTreeClassifier(random_state=241),
                 'Gradient boosting (10 trees)': GradientBoostingClassifier(n_estimators=10),
                 'Gradient boosting (50 trees)': GradientBoostingClassifier(n_estimators=50),
                 'K neighbors': KNeighborsClassifier(weights='distance', p=1, n_jobs=4),
-                'Logistic regression': LogisticRegression(C=10)
+                'Logistic regression': LogisticRegression(C=0.00001, penalty='l1', n_jobs=4)
             }
-            for name, clf in classifiers.items():
-                headers = _get_headers(self._catalog_path, indices)
-                Y = get_target_vector(self._librated_asteroids, learn_feature_set.astype(int))
-                X = get_feuture_matrix(learn_feature_set, False, indices)
+            headers = self._get_headers(indices)
+            X = get_feuture_matrix(learn_feature_set, False, indices)
 
+            scaler = StandardScaler()
+            X = scaler.fit_transform(X)
+            for name, clf in classifiers.items():
                 kf = cross_validation.KFold(X.shape[0], 5, shuffle=True, random_state=42)
                 precision, recall, accuracy, TP, FP, TN, FN = _classify(clf, kf, X, Y)
                 table.add_row([name, ', '.join(headers), precision, recall, accuracy,
