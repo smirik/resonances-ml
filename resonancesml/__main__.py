c3aaec5de00afbd48ee4e5921a65639af044f697
antlord <antlord92@gmail.com>
2016-08-29 19:30:33 +0800

    added command for comparasion valuable of parameters from catalog of synthetic elements

--- resonancesml/__main__.py
+++ resonancesml/__main__.py
@@ -13,18 +13,48 @@ def main():
 def learn(librate_list: str, catalog: str):
     from resonancesml.commands.learn import MethodComparer
     from resonancesml.commands.learn import get_tester_parameters
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.ensemble import GradientBoostingClassifier
+    from sklearn.linear_model import LogisticRegression
+
     parameters = get_tester_parameters(Catalog(catalog))
+    classifiers = {
+        'Decision tree': DecisionTreeClassifier(random_state=241),
+        'Gradient boosting (10 trees)': GradientBoostingClassifier(n_estimators=10),
+        'Gradient boosting (50 trees)': GradientBoostingClassifier(n_estimators=50),
+        'K neighbors': KNeighborsClassifier(weights='distance', p=1, n_jobs=4),
+        'Logistic regression': LogisticRegression(C=0.00001, penalty='l1', n_jobs=4)
+    }
     tester = MethodComparer(librate_list, parameters)
+    tester.set_methods(classifiers)
     tester.learn()
 
 
-@main.command()
+@main.command(name='classify-all')
 @click.option('--librate-list', '-l', type=click.Path(exists=True, resolve_path=True))
 @click.option('--all-librated', '-a', type=click.Path(exists=True, resolve_path=True))
-def classify(librate_list: str, all_librated: str):
-    from resonancesml.commands.classify import classify as _classify
-    _classify(librate_list, all_librated)
+def classify_all(librate_list: str, all_librated: str):
+    from resonancesml.commands.classify import classify_all as _classify_all
+    _classify_all(librate_list, all_librated)
 
+
+@main.command(name='compare-fields-valuable')
+@click.option('--librate-list', '-l', type=click.Path(exists=True, resolve_path=True))
+def compare_fields_valuable(librate_list: str, ):
+    from resonancesml.commands.learn import MethodComparer
+    from resonancesml.commands.learn import TesterParameters
+    from resonancesml.settings import SYN_CATALOG_PATH
+    from sklearn.neighbors import KNeighborsClassifier
+
+    parameters = TesterParameters([[2,3,4],[2,3,5],[2,4,5],[3,4,5],[2,5]],
+                                  SYN_CATALOG_PATH, 10, '  ', 2)
+    classifiers = {
+        'K neighbors': KNeighborsClassifier(weights='distance', p=1, n_jobs=4),
+    }
+    tester = MethodComparer(librate_list, parameters)
+    tester.set_methods(classifiers)
+    tester.learn()
 
 
 if __name__ == '__main__':
--- resonancesml/commands/learn.py
+++ resonancesml/commands/learn.py
@@ -1,24 +1,23 @@
 import numpy as np
-from sklearn.preprocessing import StandardScaler
 from resonancesml.shortcuts import ProgressBar
 from texttable import Texttable
-from typing import Tuple
 from typing import List
-from sklearn.base import ClassifierMixin
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import GradientBoostingClassifier
+from typing import Dict
 from resonancesml.shortcuts import get_target_vector
 from resonancesml.shortcuts import get_feuture_matrix
+from .shortcuts import perf_measure
 import pandas
 from pandas import DataFrame
-from sklearn.linear_model import LogisticRegression
 from sklearn import cross_validation
 
 from enum import Enum
 from enum import unique
 from resonancesml.settings import SYN_CATALOG_PATH
 from resonancesml.settings import CAT_CATALOG_PATH
+
+from typing import Tuple
+from sklearn.base import ClassifierMixin
+
 
 def _validate(data: DataFrame):
     flag = False
@@ -29,6 +28,12 @@ def _validate(data: DataFrame):
 
     if flag:
         raise Exception('syntetic elements has nan values')
+
+
+@unique
+class Catalog(Enum):
+    syn = 'syn'
+    cat = 'cat'
 
 
 def _classify(clf: ClassifierMixin, kf: cross_validation.KFold, X: np.ndarray, Y: np.ndarray)\
@@ -49,7 +54,7 @@ def _classify(clf: ClassifierMixin, kf: cross_validation.KFold, X: np.ndarray, Y
 
         clf.fit(X_train, Y_train)
         res = clf.predict(X_test)  # type: np.ndarray
-        TP, FP, TN, FN = _perf_measure(res, Y_test)
+        TP, FP, TN, FN = perf_measure(res, Y_test)
 
         precisions.append(TP/(TP+FP) if TP+FP != 0 else 0)
         recalls.append(TP/(TP+FN) if TP+FN != 0 else 0)
@@ -62,34 +67,6 @@ def _classify(clf: ClassifierMixin, kf: cross_validation.KFold, X: np.ndarray, Y
 
     return (np.mean(precisions), np.mean(recalls), np.mean(scores),
             np.sum(TPs), np.sum(FPs), np.sum(TNs), np.sum(FNs))
-
-
-def _perf_measure(y_actual, y_hat):
-    TP = 0
-    FP = 0
-    TN = 0
-    FN = 0
-
-    for i in range(len(y_hat)):
-        if y_actual[i]==y_hat[i]==1:
-           TP += 1
-    for i in range(len(y_hat)):
-        if y_actual[i]==1 and y_actual[i]!=y_hat[i]:
-           FP += 1
-    for i in range(len(y_hat)):
-        if y_actual[i]==y_hat[i]==0:
-           TN += 1
-    for i in range(len(y_hat)):
-        if y_actual[i]==0 and y_actual[i]!=y_hat[i]:
-           FN += 1
-
-    return(TP, FP, TN, FN)
-
-
-@unique
-class Catalog(Enum):
-    syn = 'syn'
-    cat = 'cat'
 
 
 class TesterParameters:
@@ -118,6 +95,7 @@ class MethodComparer:
             skiprows=parameters.skiprows, header=None, dtype=dtype)
         self._librated_asteroids = np.loadtxt(librate_list, dtype=int)
         self._parameters = parameters
+        self._classifiers = None  # type: Dict[str, ClassifierMixin]
 
     def _get_headers(self, by_indices: List[int]) -> List[str]:
         headers = []
@@ -133,6 +111,9 @@ class MethodComparer:
             res.append(headers[index])
         return res
 
+    def set_methods(self, values: Dict[str, ClassifierMixin]):
+        self._classifiers = values
+
     def learn(self):
         slice_len = int(self._librated_asteroids[-1])
 
@@ -143,23 +124,15 @@ class MethodComparer:
         table.set_cols_width([30, 30, 15, 15, 15, 5, 5, 5, 5])
         table.set_precision(5)
 
-        bar = ProgressBar(len(self._parameters.indices_cases) * 5, 'Learning', 1)
+        bar = ProgressBar(len(self._parameters.indices_cases) * len(self._classifiers),
+                          'Learning', 1)
         Y = get_target_vector(self._librated_asteroids, learn_feature_set.astype(int))
 
         for indices in self._parameters.indices_cases:
-            classifiers = {
-                'Decision tree': DecisionTreeClassifier(random_state=241),
-                'Gradient boosting (10 trees)': GradientBoostingClassifier(n_estimators=10),
-                'Gradient boosting (50 trees)': GradientBoostingClassifier(n_estimators=50),
-                'K neighbors': KNeighborsClassifier(weights='distance', p=1, n_jobs=4),
-                'Logistic regression': LogisticRegression(C=0.00001, penalty='l1', n_jobs=4)
-            }
             headers = self._get_headers(indices)
             X = get_feuture_matrix(learn_feature_set, False, indices)
 
-            scaler = StandardScaler()
-            X = scaler.fit_transform(X)
-            for name, clf in classifiers.items():
+            for name, clf in self._classifiers.items():
                 kf = cross_validation.KFold(X.shape[0], 5, shuffle=True, random_state=42)
                 precision, recall, accuracy, TP, FP, TN, FN = _classify(clf, kf, X, Y)
                 table.add_row([name, ', '.join(headers), precision, recall, accuracy,
